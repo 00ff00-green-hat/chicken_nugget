991bc1a45fc3326c0d64cf83d2bcc980a478d3da
GreenHat-0f0 <kiara.paasrosa@gmail.com>
2026-06-24 23:36:27 -0300

    khbfcyg

---
+++ feet3/3ativ 1.py
@@ -0,0 +1,4 @@
+# 1 - Implemente um programa com um cadastro de idades de 6 alunos utilizando lista. O
+# programa deve solicitar as idades dos 6 alunos. Após informar todas as idades, deve-se
+# apresentar apenas as idades que forem maiores ou iguais a 16.
+alunos = []
---
+++ feet3/3ativ 2.py
@@ -0,0 +1,5 @@
+a=int(input("Digite a primeira nota: "))
+b=int(input("Digite a segunda nota: "))
+c=int(input("Digite a terceira nota: "))
+d=int(input("Digite a quarta nota: "))
+notas=[a, b, c, d]
